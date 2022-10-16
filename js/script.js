cc6b408a158bda573d282d6acc140177ad2fcaca
munarabaisalova <munarabaisalova12@gmail.com>
2022-10-16 14:22:46 -0900

    init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+stardswiper = new Swiper(".mySwiper", {
+    effect: "flip",
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
